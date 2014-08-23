7248855b7db8497c1ccee6c611fd9b9b5981b38c
Sebastian Godoy <sgodoy@gmail.com>
2014-08-23 21:09:16 +0000

    Initial commit with one LED working

---
+++ LED-Test01/ledTest01.py
@@ -0,0 +1,18 @@
+# First test for the Rpi. I want to turn one LED on and off each second
+#
+# Sebastian Godoy
+# 2014/08/23
+
+import RPi.GPIO as gpio
+import time
+
+# Set up the pins to be like the board numbering
+gpio.setmode(gpio.BOARD)
+
+#Label the pin with the First LED and define it as an output
+led = 3
+gpio.setup(led,gpio.OUT)
+
+gpio.output(led,1)
+time.sleep(1)
+gpio.output(led,0)
